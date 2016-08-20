08a51b2240001f37510dc035a67bcca755d2e3e9
Kazuki Oishi <oishikazuki@gmail.com>
2016-08-20 16:45:53 +0900

    mikutter_botが表示できなくなるバグを修正

--- mikutter_wakaba.rb
+++ mikutter_wakaba.rb
@@ -13,17 +13,21 @@ Plugin.create(:mikutter_wakaba){
       render_background_orig(context)
       #初心ツイッタラー標識をつける(アカウント取得から1年未満の者に表示する)
       #1年=31536000sec
-      if (Time.now - message[:user][:created]) >= 31536000
-        return
+      #垢の作成日時が参照できないとエラーで描画が全部落ちることが判明したので、例外処理にて対応
+      begin
+        if (Time.now - message[:user][:created]) >= 31536000
+          return
+        end
+        if !@@wakaba
+          Gdk::MiraclePainter::load_wakaba()
+        end
+        context.save {
+          context.translate(width - @@wakaba.width, (height - @@wakaba.height) >= 0 ? height - @@wakaba.height : 0)
+          context.set_source_pixbuf(@@wakaba)
+          context.paint(0.2)
+        }
+      rescue
       end
-      if !@@wakaba
-        Gdk::MiraclePainter::load_wakaba()
-      end
-      context.save {
-        context.translate(width - @@wakaba.width, (height - @@wakaba.height) >= 0 ? height - @@wakaba.height : 0)
-        context.set_source_pixbuf(@@wakaba)
-        context.paint(0.2)
-      }
     end
 
   end
